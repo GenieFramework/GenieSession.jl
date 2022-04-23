9ac490a9799c350b73d955ad81f42b985098cef0
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-04-23 17:11:50 +0200

    initial commit

---
+++ src/Flash.jl
@@ -0,0 +1,44 @@
+"""
+Various utility functions for using across models, controllers and views.
+"""
+module Flash
+
+import Genie
+import GenieSession
+
+export flash, flash_has_message
+
+
+"""
+    flash()
+
+Returns the `flash` dict object associated with the current HTTP request.
+"""
+function flash()
+  get(Genie.Requests.payload(), Genie.Router.PARAMS_FLASH_KEY, "")
+end
+
+
+"""
+    flash(value::Any) :: Nothing
+
+Stores `value` onto the flash.
+"""
+function flash(value::Any) :: Nothing
+  GenieSession.set!(GenieSession.session(Genie.Requests.payload()), Genie.Router.PARAMS_FLASH_KEY, value)
+  Genie.Requests.payload()[Genie.Router.PARAMS_FLASH_KEY] = value
+
+  nothing
+end
+
+
+"""
+    flash_has_message() :: Bool
+
+Checks if there's any value on the flash storage
+"""
+function flash_has_message() :: Bool
+  ! isempty(flash())
+end
+
+end
